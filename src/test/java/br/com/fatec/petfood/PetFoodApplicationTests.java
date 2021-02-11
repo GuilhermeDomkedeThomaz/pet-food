2d6a437cefebd9362ec790f06dceb82f1349761d
Guilherme Domke de Thomaz <gthomaz@uolinc.com>
2021-02-10 21:41:08 -0300

    Guilherme - First commit - init project and include swagger config

---
+++ src/test/java/br/com/fatec/petfood/PetFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.petfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
